93f11232e03fe0def1af7bd24b34adf09ccbc650
Cecato <gustavocecato010620@gmail.com>
2023-09-16 19:15:15 -0300

    login check

--- src/authentication/login.py
+++ src/authentication/login.py
@@ -1,4 +1,5 @@
 import os
+import time
 from dotenv import load_dotenv
 from selenium.webdriver.common.by import By
 
@@ -6,24 +7,46 @@ load_dotenv()
 EMAIL = os.getenv('EMAIL')
 PASSWORD = os.getenv('PASSWORD')
 
+
 def login(driver):
-    title = driver.title
-    assert 'LinkedIn: entre ou cadastre-se' in title
+    try:
+        title = driver.title
+        assert 'LinkedIn: entre ou cadastre-se' in title
 
-    driver.implicitly_wait(0.5)
+        driver.implicitly_wait(0.5)
 
-    text_box_email = driver.find_element(by=By.ID, value="session_key")
-    text_box_email.send_keys(EMAIL)
+        text_box_email = driver.find_element(by=By.ID, value="session_key")
+        text_box_email.send_keys(EMAIL)
 
-    text_box_password = driver.find_element(by=By.ID,  value="session_password")
-    text_box_password.send_keys(PASSWORD)
+        text_box_password = driver.find_element(by=By.ID,  value="session_password")
+        text_box_password.send_keys(PASSWORD)
 
-    submit_button = driver.find_elements(by=By.CSS_SELECTOR, value="button")
-    x = 0
-    for button in submit_button:
-        if button.text == "Entrar":
-            submit_button = submit_button[x]
-            break
-        x = x+1
+        submit_button = driver.find_elements(by=By.CSS_SELECTOR, value="button")
+        x = 0
+        for button in submit_button:
+            if button.text == "Entrar":
+                submit_button = submit_button[x]
+                break
+            x = x+1
 
-    submit_button.click() 
+        submit_button.click()
+
+        check_login(driver)
+
+    except Exception as e:
+        print('Error', format(e))
+
+def check_login(driver):
+
+    try:
+        driver.implicitly_wait(0.5)
+        box_check = driver.find_element(by=By.ID, value="securedDataExchange")
+
+        while box_check is not None:
+            print("Resolva a catch")
+            time.sleep(1)
+            box_check = driver.find_element(by=By.ID, value="securedDataExchange")
+    
+    except:
+        pass
+
--- src/main.py
+++ src/main.py
@@ -1,28 +1,23 @@
 #libraries
 import time
-import sys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 #files
 from authentication.login import login
-from utils.vagas import clickar_vagas
+
 
 driver = webdriver.Chrome()
 driver.maximize_window()
 driver.get("https://www.linkedin.com/")
 
+def execute():
 
-try:
-    
     login(driver)
-    
-    clickar_vagas(driver)
 
-    time.sleep(50)
-    driver.quit()
-except Exception as e:
-    print('Error', format(e))
+    time.sleep(500000)
     driver.quit()
 
-sys.exit()
+
+execute()
+
